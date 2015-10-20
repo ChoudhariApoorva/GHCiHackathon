54eb8e409d69f8b39f551041267b7fd683362158
neha21h <neha21h@gmail.com>
2015-10-20 22:50:59 +0530

    Adding proper ids

--- Frontend/js/CreateRequest.js
+++ Frontend/js/CreateRequest.js
@@ -1,24 +1,24 @@
 $(document).ready(function(e){
 
-    $('#login-form-link').click(function(e) {
+    $('#contact-details-tab').click(function(e) {
 		$("#login-form").delay(100).fadeIn(100);
  		$("#register-form").fadeOut(100);
-		$('#register-form-link').removeClass('active');
+		$('#request-details-tab').removeClass('active');
 		$(this).addClass('active');
 		e.preventDefault();
 	});
-	$('#register-form-link').click(function(e) {
+	$('#request-details-tab').click(function(e) {
 		$("#register-form").delay(100).fadeIn(100);
  		$("#login-form").fadeOut(100);
-		$('#login-form-link').removeClass('active');
+		$('#contact-details-tab').removeClass('active');
 		$(this).addClass('active');
 		e.preventDefault();
 	});
-	$('#ContactTab-NextButton').click(function()
+	$('#ContactTab-NextButton').click(function(e)
 	{
 		$("#register-form").delay(100).fadeIn(100);
  		$("#login-form").fadeOut(100);
-		$('#login-form-link').removeClass('active');
+		$('#contact-details-tab').removeClass('active');
 		$(this).addClass('active');
 		e.preventDefault();
 	});
