274bab5201133abdd043f92d08f7e50932de45d9
Apoorva Choudhari <apoorva.c17@gmail.com>
2015-10-20 23:19:51 +0530

    CreateRequest.js

--- Frontend/js/CreateRequest.js
+++ Frontend/js/CreateRequest.js
@@ -23,4 +23,27 @@ $(document).ready(function(e){
 		e.preventDefault();
 	});
 
+	$('#RequestTab-Submit').click(function(e)
+	{
+		var name = $('#inputFullName').val();
+		var emailId = $('#inputEmailId').val();
+		var mobileNumber = $('#inputMobileNumber').val();
+		var gender = $('#radioMale').val();
+		var dateOfBirth = $('#inputDOB').val();
+		var city = $('#inputCity').val();
+		var state = $('#inputState').val();
+
+		var type = $('#radioOfferingAid').val();
+		var condition = $('#select').val();
+		var requestName = $('#inputRequestName').val(); 	
+		var requiredBy = $('#inputReqByDate').val(); 	
+
+		$.post(" http://localhost:8080/antardrishti/create",
+		{
+        	name:name,
+        	emailId:emailId
+    	});
+		
+	});
+
 });
