7a7e4a4b035403f39a010de07eb35e2c34416fa1
Apoorva Choudhari <apoorva.c17@gmail.com>
2015-10-31 12:57:37 +0530

    Create Request php

---
+++ Backend/createRequest.php
@@ -0,0 +1,11 @@
+<?php
+	$data = array('name' => "Harry", 
+				  'email' => "harry@hogwarts.com",
+				  'number' => "9824774147",
+				  'gender' => "Male",
+				  'dob' => "18-09-1990",
+				  'city' => "London",
+				  'state' => "UK",
+				  	);
+	echo json_encode($data);
+?>
