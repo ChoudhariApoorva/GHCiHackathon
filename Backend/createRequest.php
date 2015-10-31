bf9a008bcbe402e4e6d02acb9acbe18742e006d3
neha21h <neha21h@gmail.com>
2015-10-31 19:29:28 +0530

    Create req php

--- Backend/createRequest.php
+++ Backend/createRequest.php
@@ -1,11 +1,36 @@
 <?php
-	$data = array('name' => "Harry", 
-				  'email' => "harry@hogwarts.com",
-				  'number' => "9824774147",
-				  'gender' => "Male",
-				  'dob' => "18-09-1990",
-				  'city' => "London",
-				  'state' => "UK",
+	session_start();
+	header("Access-Control-Allow-Origin:*");
+	
+	$servername = 'localhost';
+	$username = 'root';
+	$pwd = 'root';
+	$dbname = 'HelpingHand';
+	
+	
+	// Create connection
+    $conn = mysqli_connect($servername, $username, $pwd, $dbname);
+	// Check connection
+	if (!$conn) {
+		die("Connection failed: " . mysqli_connect_error());
+	}
+	
+	$userID = $_SESSION['UserID'];
+	$sql = "SELECT * FROM USERS WHERE UserID = '$userID'";
+	
+	if($result = mysqli_query($conn, $sql))
+	{
+		$row = mysqli_fetch_row($result);
+	
+		$data = array('name' => $row[1], 
+				  'email' => $row[2],
+				  'number' => $row[3],
+				  'gender' => $row[4],
+				  'dob' => $row[5],
+				  'city' => $row[6],
+				  'state' => $row[7],
 				  	);
-	echo json_encode($data);
+		echo json_encode($data);
+	}
+	
 ?>
